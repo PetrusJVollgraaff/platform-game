81f560b1e5a36ba50722cfc125bd0956c01f2930
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2024-03-04 09:49:21 +0200

    Update

--- game.py
+++ game.py
@@ -17,50 +17,17 @@ Player_vel = 5
 
 pygame.font.init()
 
-def handle_vertical_collision(player, objects, dy):
-    collided_obj = []
-
-    for obj in objects:
-        if player.rect.colliderect(obj.rect):
-            if dy > 0:
-                player.rect.bottom = obj.rect.top
-                player.landed()
-            elif dy < 0:
-                player.rect.top = obj.rect.bottom
-                player.hit_head()
-
-        collided_obj.append(obj)
-    return collided_obj
-
-def handle_horizontal_collision(player, objects, dx):
-    player.move(dx, 0)
-    #player.update()
-    collided_obj = None
-
-    for obj in objects:
-        if player.rect.colliderect(obj.rect):
-            collided_obj = obj
-            break
-
-
-    player.move(-dx, 0)
-    #player.update()
-    return collided_obj
-
 def draw_text(win, text, color, top, left, font ):
     label = font.render(text, 1, color)
 
     win.blit(label, (top - (label.get_width() / 2), left - (label.get_height() / 2)))
 
-def playerhandler(player, objects):
+def playerhandler(player, map):
     keys = pygame.key.get_pressed()
     player.vel_x = 0
 
-    #collide_left = handle_horizontal_collision(player, objects, -Player_vel * 3)
-    #collide_right = handle_horizontal_collision(player, objects, Player_vel * 3)
-
-    #collide_left = objects.horizontal_collision(player, -Player_vel * 3)
-    #collide_right = objects.horizontal_collision(player, Player_vel * 3)
+    #collide_left = map.horizontal_collision(player, -Player_vel * 3)
+    #collide_right = map.horizontal_collision(player, Player_vel * 3)
 
     if keys[pygame.K_LEFT]:
         player.move_left(Player_vel)
@@ -68,13 +35,10 @@ def playerhandler(player, objects):
     if keys[pygame.K_RIGHT]:
         player.move_right(Player_vel)
 
-    #vertical_collide = handle_vertical_collision(player, objects, player.vel_y)
-    vertical_collide = objects.vertical_collision(player, player.vel_y)
+    vertical_collide = map.vertical_collision(player, player.vel_y)
 
-def draw_onboard(win, player, objects, offset_x):
-    #for block in objects:
-    #    block.draw(win, offset_x)
-    objects.draw(win, offset_x)
+def draw_onboard(win, player, map, offset_x):
+    map.draw(win, offset_x)
 
     player.draw(win, offset_x)
     pygame.display.update()
@@ -83,13 +47,8 @@ def main(win):
     clock = pygame.time.Clock()
     run = True
     floor_size = 64
-    #floor = [Ground(i * floor_size, screen_height - floor_size, floor_size)
-    #         for i in range(-screen_width // floor_size, (screen_width * 2) // floor_size)]
 
-    #wall = [Ground(550, screen_height - (floor_size * 3), floor_size)]
     player = Player(20, screen_height - floor_size + 2)
-    #ground = Ground(150, 150, )
-    #objects = [*floor, Ground(500, screen_height - (floor_size * 2), floor_size)]
     level_map = Level1()
     level_map.ground_blocks(floor_size)
 
